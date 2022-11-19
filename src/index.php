5c4f9b3723b6c839d868a35f6a4f2597b15ec376
Barna Gergely <h144988@stud.u-szeged.hu>
2022-11-19 16:12:48 +0100

    stílis javítása

--- src/index.php
+++ src/index.php
@@ -1,25 +1,27 @@
 <?php
 require_once('head.html');
 ?>
-<h1>Üdvözlünk a könyvtárban!</h1>
+<section class="container">
+    <h1>Üdvözlünk a könyvtárban!</h1>
 
-<h2>Módosítások a sablonon: </h2>
-<ul>
-    <li>Adatbázis kapcsolat megjavítása (hibás volt a sablon karakter készlete)</li>
-    <li>Adatbázis olvasás hiba függvényének cseréje (mysqli-t használunk, aminek nincs mysql_error() függvénye, ezért mysqli_error($conn) -t írtam a helyére)</li>
-    <li>menü és footer újra alkotása (head footer)</li>
-    <li>stílus létrehozása</li>
-    <li>Logo beillesztése</li>
-</ul>
+    <h2>Módosítások a sablonon: </h2>
+    <ul>
+        <li>Adatbázis kapcsolat megjavítása (hibás volt a sablon karakter készlete)</li>
+        <li>Adatbázis olvasás hiba függvényének cseréje (mysqli-t használunk, aminek nincs mysql_error() függvénye, ezért mysqli_error($conn) -t írtam a helyére)</li>
+        <li>menü és footer újra alkotása (head footer)</li>
+        <li>stílus létrehozása</li>
+        <li>Logo beillesztése</li>
+    </ul>
 
-<h2>TODO: </h2>
-<ul>
-    <li>Tesztelni, hogy megfelelően működik e a mysqli_error (nem tudom tényleg jó e)</li>
-    <li>Személyre szabni a menüt (legalább hogy merre van zárva vagy ilyesmi)</li>
-    <li>Személyre szabní a stílust (üresen hagytam egy csomó mindent, amit hibaként ír a style.css)</li>
-    <li>személyre szabni a kommenteket, függvényeket, fájlneveket, mappa rendszert, stb., hogy ne annyira látszódjon hogy másolt</li>
-    <li>Megírni a lényegi működését a programnak</li>
-</ul>
+    <h2>TODO: </h2>
+    <ul>
+        <li>Tesztelni, hogy megfelelően működik e a mysqli_error (nem tudom tényleg jó e)</li>
+        <li>Személyre szabni a menüt (legalább hogy merre van zárva vagy ilyesmi)</li>
+        <li>Személyre szabní a stílust (üresen hagytam egy csomó mindent, amit hibaként ír a style.css)</li>
+        <li>személyre szabni a kommenteket, függvényeket, fájlneveket, mappa rendszert, stb., hogy ne annyira látszódjon hogy másolt</li>
+        <li>Megírni a lényegi működését a programnak</li>
+    </ul>
+</section>
 
 <?php
 require_once('footer.html');
--- src/kolcsonzesek.php
+++ src/kolcsonzesek.php
@@ -1,82 +1,86 @@
-<?php 
+<?php
 require_once('head.html');
 ?>
-	
 
-<h1>Új kölcsönzés</h1>
+<section class="container">
+	<h1>Új kölcsönzés</h1>
 
-<form method="POST" action="kolcsonzesfelvitel.php" accept-charset="utf-8">
-<label>Könyv : </label>
-<select name="valasztottKonyv">
-<?php 
-	$szabadKonyvek = szabad_konyveket_listaz();
-	while( $egySor = mysqli_fetch_assoc($szabadKonyvek) ) {
-		echo '<option value="'.$egySor["konyvszam"].'">'.$egySor["konyv"].'</option>';
-	}
-	mysqli_free_result($szabadKonyvek);
+	<form method="POST" action="kolcsonzesfelvitel.php" accept-charset="utf-8">
+		<label>Könyv : </label>
+		<select name="valasztottKonyv">
+			<?php
+			$szabadKonyvek = szabad_konyveket_listaz();
+			while ($egySor = mysqli_fetch_assoc($szabadKonyvek)) {
+				echo '<option value="' . $egySor["konyvszam"] . '">' . $egySor["konyv"] . '</option>';
+			}
+			mysqli_free_result($szabadKonyvek);
 
-?>
-</select>
-<br>
-<label>Könyv címe: </label>
-<select name="olvaso">
-<?php 
-	$olvasok = olvasolistatLeker();
-	while( $egySor = mysqli_fetch_assoc($olvasok) ) {
-		echo '<option value="'.$egySor["olvasojegy"].'">'. 
-		      $egySor["olvasojegy"]. ' - '. 
-		      $egySor["nev"]. ', ' . 
-		      $egySor["lakcim"] .'</option>';
-	}
-	mysqli_free_result($olvasok);
+			?>
+		</select>
+		<br>
+		<label>Könyv címe: </label>
+		<select name="olvaso">
+			<?php
+			$olvasok = olvasolistatLeker();
+			while ($egySor = mysqli_fetch_assoc($olvasok)) {
+				echo '<option value="' . $egySor["olvasojegy"] . '">' .
+					$egySor["olvasojegy"] . ' - ' .
+					$egySor["nev"] . ', ' .
+					$egySor["lakcim"] . '</option>';
+			}
+			mysqli_free_result($olvasok);
 
-?>
-</select>
-<br>
-<input type="submit" value="Elküld" />
+			?>
+		</select>
+		<br>
+		<input type="submit" value="Elküld" />
 
-</form>
+	</form>
 
 
-<hr/>
-<h1>Kölcsönzött könyvek listája</h1>
+	<hr />
+</section>
 
-<table border="1">
-<tr>
-<th>Könyvszám</th>
-<th>Cím</th>
-<th>Szerző</th>
-<th>Kiadó</th>
-<th>Év</th>
-<th>Olvasója</th>
-</tr>
+<section class="container">
+	<h1>Kölcsönzött könyvek listája</h1>
 
-<?php
+	<table border="1">
+		<tr>
+			<th>Könyvszám</th>
+			<th>Cím</th>
+			<th>Szerző</th>
+			<th>Kiadó</th>
+			<th>Év</th>
+			<th>Olvasója</th>
+		</tr>
 
-	$konyvek = kolcsonzott_konyvek_listaja(); // ez egy eredményhalmazt ad vissza
-	
-	// soronként dolgozzuk fel az eredményt
-	// minden sort egy asszociatív tömbben kapunk meg
-    while( $egySor = mysqli_fetch_assoc($konyvek) ) { 
-		echo '<tr>';
-		echo '<td>'. $egySor["konyvszam"] .'</td>';
-		echo '<td>'. $egySor["cim"] .'</td>';
-		echo '<td>'. $egySor["szerzo"] .'</td>';
-		echo '<td>'. $egySor["kiado"] .'</td>';
-		echo '<td>'. $egySor["ev"] .'</td>';
-		echo '<td>'. $egySor["olvaso"] .'</td>';
-		echo '<td><form method="POST" action="kolcsonzestorles.php">
-				  <input type="hidden" name="toroltkonyv" value="'.$egySor["konyvszam"].'" />
+		<?php
+
+		$konyvek = kolcsonzott_konyvek_listaja(); // ez egy eredményhalmazt ad vissza
+
+		// soronként dolgozzuk fel az eredményt
+		// minden sort egy asszociatív tömbben kapunk meg
+		while ($egySor = mysqli_fetch_assoc($konyvek)) {
+			echo '<tr>';
+			echo '<td>' . $egySor["konyvszam"] . '</td>';
+			echo '<td>' . $egySor["cim"] . '</td>';
+			echo '<td>' . $egySor["szerzo"] . '</td>';
+			echo '<td>' . $egySor["kiado"] . '</td>';
+			echo '<td>' . $egySor["ev"] . '</td>';
+			echo '<td>' . $egySor["olvaso"] . '</td>';
+			echo '<td><form method="POST" action="kolcsonzestorles.php">
+				  <input type="hidden" name="toroltkonyv" value="' . $egySor["konyvszam"] . '" />
 				  <input type="submit" value="Kölcsönzés torlése" />
 		          </form></td>';
-		echo '</tr>';
-	} 
-	mysqli_free_result($konyvek); // töröljük a listát a memóriából
+			echo '</tr>';
+		}
+		mysqli_free_result($konyvek); // töröljük a listát a memóriából
 
-?>
-</table>
+		?>
+	</table>
+</section>
 
 
-<?php 
+<?php
 require_once('footer.html');
 ?>
